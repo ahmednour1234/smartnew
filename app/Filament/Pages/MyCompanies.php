62e1c0743bd53df7c0ddc6d004e8e6c157a23c3e
ahmednour1234 <ahmednour5999@gmail.com>
2026-02-15 11:07:49 +0200

    init

---
+++ app/Filament/Pages/MyCompanies.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use App\Filament\Resources\CompanyResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Auth;
+
+class MyCompanies extends ListRecords
+{
+    protected static string $resource = CompanyResource::class;
+
+    protected static ?string $navigationLabel = 'My Companies';
+
+    protected static ?string $navigationIcon = 'heroicon-o-user-circle';
+
+    protected static ?int $navigationSort = 2;
+
+    public static function canAccess(): bool
+    {
+        return Auth::user()?->hasPermission('view_company') ?? false;
+    }
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+
+    protected function getTableQuery(): Builder
+    {
+        $query = CompanyResource::getEloquentQuery();
+        $query->withoutGlobalScopes();
+        return $query->where('user_id', Auth::id());
+    }
+}
