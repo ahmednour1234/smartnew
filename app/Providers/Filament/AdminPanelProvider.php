5e94ede3d4bbe4f072f4a23c60b295252bb29cb3
ahmednour1234 <ahmednour5999@gmail.com>
2026-02-17 14:06:15 +0200

    init

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,7 +25,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('public/admin')
+            ->path('admin')
             ->login()
             ->brandName('Sales Of Smart')
             ->colors([
