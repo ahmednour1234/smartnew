38cad31c9f099c4079ae114ea0aa8f3f8f337dd8
ahmednour1234 <ahmednour5999@gmail.com>
2026-02-22 14:55:17 +0200

    init

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Livewire\Livewire;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,16 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Override Livewire's update endpoint to always use /public prefix
-        Livewire::setUpdateRoute(function ($handle) {
-            return \Illuminate\Support\Facades\Route::post('/public/livewire/update', $handle)
-                ->middleware(['web']);
-        });
-
-        // Override Livewire's script route to use /public prefix
-        Livewire::setScriptRoute(function ($handle) {
-            return \Illuminate\Support\Facades\Route::get('/public/livewire/livewire.js', $handle)
-                ->middleware(['web']);
-        });
+        //
     }
 }
